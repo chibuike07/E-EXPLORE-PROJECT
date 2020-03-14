836bb675b83ced83dabc969b1f594f2667c8d66d
Daniel2code <danielnwoke20@gmail.com>
2020-03-14 18:51:31 +0100

    service page commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
-import ContactPage from "./Component/Contact page/Parent";
+// import ContactPage from "./Component/Contact page/Parent";
+import Service from './Component/Services'
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<ContactPage />, document.getElementById("root"));
+ReactDOM.render(<Service />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
